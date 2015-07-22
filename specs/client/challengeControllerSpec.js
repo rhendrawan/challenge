ccc0b96d17eae89e9aafc25f4a1de28e0efacf3f
mjsgreen01 <mjsgreen01@gmail.com>
2015-07-22 10:22:53 -0700

    [feat] attempting to add service tests for challenge

--- specs/client/challengeControllerSpec.js
+++ specs/client/challengeControllerSpec.js
@@ -12,5 +12,41 @@ describe('ChallengeController', function(){
 
   });
 
+  // describe('create-challenge', function () {
+
+  //   var CreateChallengeFactory, $httpBackend;
+
+  //   beforeEach(inject(function (_CreateChallengeFactory_, _$httpBackend_) {
+  //     CreateChallengeFactory = _CreateChallengeFactory_;
+  //     $httpBackend = _$httpBackend_;
+  //   }));
+
+  //   it('creates the challenge on the server', function () {
+  //     $httpBackend
+  //     .expectPOST('/api/1/challenge', {
+  //       'title':'testTitle', 
+  //       'message':'testMessage',
+  //       'participants':[{'id':2}],
+  //       'wager':'testWager'
+  //     })
+  //     .respond(200);
+
+  //     var succeeded;
+  //     CreateChallengeFactory.postChallenge({
+  //       'title':'testTitle', 
+  //       'message':'testMessage',
+  //       'participants':[{'id':2}],
+  //       'wager':'testWager'
+  //     }).then(function(){
+  //       succeeded = true;
+  //     });
+  //     $httpBackend.flush();
+  //     expect(succeeded).to.be.true;
+
+      
+  //   });
+
+  // });
+
 
 });
