8350aff81232d72caf07c2bee77890415df354b8
eatpie75 <eatpie75@gmail.com>
2015-07-26 14:13:57 -0700

    [refactor] change defaults on challengeFilter to null

--- public/angular/client/challengerApp/app.js
+++ public/angular/client/challengerApp/app.js
@@ -90,21 +90,21 @@ angular.module('challengeApp', [
   return function(input, accepted, started, complete, user) {
     user = (user !== undefined) ? parseInt(user) : undefined;
     accepted = (accepted !== undefined && user !==undefined) ? !!parseInt(accepted) : null;
-    started = (started !== undefined) ? !!parseInt(started) : false;
-    complete = (complete !== undefined) ? !!parseInt(complete): false;
+    started = (started !== undefined) ? !!parseInt(started) : null;
+    complete = (complete !== undefined) ? !!parseInt(complete): null;
 
     return input.filter(function(challenge) {
-      var has_accepted;
+      var has_accepted = true;
 
       if (accepted !== null) {
         has_accepted = challenge.participants.some(function(participant) {
           return (participant.accepted === accepted && participant.id === user);
         });
-      } else {
-        has_accepted = true;
       }
 
-      return (challenge.started === started && challenge.complete === complete && has_accepted);
+      return (
+        ((challenge.started === started) || started === null) && ((challenge.complete === complete) || complete === null) && has_accepted
+      );
     });
   };
 }).directive('challengeTable', function() {
@@ -120,4 +120,4 @@ angular.module('challengeApp', [
     'restrict': 'E',
     'templateUrl': '/angular/client/challengerApp/user/challengeTable.html'
   };
-});;
+});
