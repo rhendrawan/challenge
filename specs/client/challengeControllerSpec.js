fc5dd9f034258c8e9cf4c35a3b2c80559ab54ae1
mjsgreen01 <mjsgreen01@gmail.com>
2015-07-27 08:21:25 -0700

    change karma config to use ui router

--- specs/client/challengeControllerSpec.js
+++ specs/client/challengeControllerSpec.js
@@ -12,41 +12,45 @@ describe('ChallengeController', function(){
 
   });
 
-  // describe('create-challenge', function () {
+  describe('create-challenge', function () {
 
-  //   var CreateChallengeFactory, $httpBackend;
+    var CreateChallengeFactory, CreateChallengeController, $httpBackend;
 
-  //   beforeEach(inject(function (_CreateChallengeFactory_, _$httpBackend_) {
-  //     CreateChallengeFactory = _CreateChallengeFactory_;
-  //     $httpBackend = _$httpBackend_;
-  //   }));
+    beforeEach(inject(function (_CreateChallengeFactory_, _CreateChallengeController_, _$httpBackend_) {
+      CreateChallengeFactory = _CreateChallengeFactory_;
+      CreateChallengeController = _CreateChallengeController_;
+      $httpBackend = _$httpBackend_;
+    }));
 
-  //   it('creates the challenge on the server', function () {
-  //     $httpBackend
-  //     .expectPOST('/api/1/challenge', {
-  //       'title':'testTitle', 
-  //       'message':'testMessage',
-  //       'participants':[{'id':2}],
-  //       'wager':'testWager'
-  //     })
-  //     .respond(200);
+    it('creates the challenge and responds with challenge ID', function () {
+      $httpBackend
+      .expectPOST('/api/1/challenge', {
+        'title':'testTitle', 
+        'message':'testMessage',
+        'participants':[{'id':2}],
+        'wager':'testWager'
+      })
+      .respond(200, {'id':2});
 
-  //     var succeeded;
-  //     CreateChallengeFactory.postChallenge({
-  //       'title':'testTitle', 
-  //       'message':'testMessage',
-  //       'participants':[{'id':2}],
-  //       'wager':'testWager'
-  //     }).then(function(){
-  //       succeeded = true;
-  //     });
-  //     $httpBackend.flush();
-  //     expect(succeeded).to.be.true;
+      var challengeID;
+      CreateChallengeFactory.postChallenge({
+        'title':'testTitle', 
+        'message':'testMessage',
+        'participants':[{'id':2}],
+        'wager':'testWager'
+      }).then(function(res){
+        challengeID = res.id;
+      });
+      $httpBackend.flush();
+      expect(challengeID).to.equal(2);
 
       
-  //   });
+    });
 
-  // });
+    // use sinon.spy to make sure 'CreateChallengeFactory.postChallenge' is called when
+    // CreateChallengeController.postChallenge runs
+
+  });
 
 
 });
